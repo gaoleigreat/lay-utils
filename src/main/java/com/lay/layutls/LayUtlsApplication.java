6d7255401c657c947e50af11a904b7ad7c77db76
lay <513684652@qq.com>
2023-01-03 17:15:22 +0800

    first commit

---
+++ src/main/java/com/lay/layutls/LayUtlsApplication.java
@@ -0,0 +1,13 @@
+package com.lay.layutls;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LayUtlsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LayUtlsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lay/layutls/LayUtlsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lay.layutls;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LayUtlsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
