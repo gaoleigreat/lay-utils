056c1f4171a684fbe4ae0ab3d3e5b106100a3d28
lay <513684652@qq.com>
2023-02-16 11:49:28 +0800

    add flyway

--- src/main/java/com/lay/layutls/FlywayConfig.java
+++ src/main/java/com/lay/layutls/FlywayConfig.java
@@ -1,55 +1,38 @@
-
-/*
- * Copyright 2021 Wicrenet, Inc. All rights reserved.
- */
-package com.lay.layutls;
-
-/**
- * 【 flyway配置 自动生成flyway_schema_history表 】
- *
- * @author yangjunxiong
- * Created on 2021/6/25 17:08
- */
-
-import org.flywaydb.core.Flyway;
-import org.flywaydb.core.api.FlywayException;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Configuration;
-
-import javax.annotation.PostConstruct;
-import javax.sql.DataSource;
-
 @Configuration
+@EnableTransactionManagement
+@Import({DynamicDataSourceRegister.class})
 public class FlywayConfig {
-    @Autowired
-    private DataSource dataSource;
-    private Logger     logger = LoggerFactory.getLogger(this.getClass());
+    private static final String BASE_SQL_LOCATION = "/db/baseMigration";
+    private static final String ENCODING = "UTF-8";
 
-    @PostConstruct
+    // application配置文件中base数据源配置
+    @Value("${frame.druid.ds.base.url}")
+    private string baseUrl;
+    @Value("${frame.druid.ds.base.username}")
+    private string baseUserName;
+    @Value("${frame.druid.ds.base.password}")
+    private string basePassword;
+    @Value("${frame.druid.ds.base.driverClassName}")
+    private string baseDriverClassName;
+
+    @Bean
     public void migrate() {
         Flyway flyway = Flyway.configure()
-                .dataSource(dataSource)
-                .locations("db/migration")//sql文件名称规则："V20210625.17.30__V1.0.sql"
+                .dataSource(getBaseDataSource())
+                .locations(BASE_SQL_LOCATION)
+                .encoding(ENCODING)
                 .baselineOnMigrate(true)
-                .outOfOrder(true)//对于开发环境, 可能是多人协作开发, 很可能先 apply 了自己本地的最新 SQL 代码, 然后发现其他同事早先时候提交的 SQL 代码还没有 apply, 所以 开发环境应该设置 spring.flyway.outOfOrder=true, 这样 flyway 将能加载漏掉的老版本 SQL 文件; 而生产环境应该设置 spring.flyway.outOfOrder=false
-                .encoding("UTF-8")
-                .load()
-        try {
-            flyway.migrate();
-        } catch (FlywayException e) {
-            logger.error("Flyway配置第一次加载出错", e);
-            try {
-                flyway.repair();//生成版本记录表
-                logger.info("Flyway配置修复成功");
-                flyway.migrate();
-                logger.info("Flyway配置重新加载成功");
-            } catch (Exception e1) {
-                logger.error("Flyway配置第二次加载出错", e1);
-                throw e1;
-            }
-        }
+                .table("base_flyway")
+                .load();
+        flyway.migrate();
+    }
+
+    private BasicDataSource getBaseDataSource() {
+        BasicDataSource dataSource = new BasicDataSource();
+        dataSource.setUrl(baseUrl);
+        dataSource.setUserName(baseUserName);
+        dataSource.setPassword(basePassword);
+        dataSource.setDriverClassName(baseDriverClassName);
+        return dataSource;
     }
 }
-
